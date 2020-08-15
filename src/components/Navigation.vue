f99e2d8734bd969d1d863436446e1789b00fb430
Ahamed Althaf <nicolain2m@gmail.com>
2020-08-16 00:54:29 +0530

    responsive nav bar

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,16 +1,13 @@
 <template>
   <nav class="nav-container">
     <Logo/>
-    <ul>
-      <li><g-link to="/activities">Activities</g-link>
-        <nav>
-          <ul>
-            <li><g-link to="/activities/education">Education</g-link></li>
-            <li><g-link to="/activities/entrepreneurship">Entrepreneurship</g-link></li>
-            <li><g-link to="/activities/general">General</g-link></li>
-          </ul>
-        </nav>
-      </li>
+    <button class="hamburger" @click="doSomething">
+      <svg viewBox="0 0 24 24" width="24" height="24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" class="css-i6dzq1"><line x1="3" y1="12" x2="21" y2="12"></line><line x1="3" y1="6" x2="21" y2="6"></line><line x1="3" y1="18" x2="21" y2="18"></line></svg>
+    </button>
+    <ul :class="{open: open}">
+      <li><g-link to="/education">Education</g-link></li>
+      <li><g-link to="/entrepreneurship">Entrepreneurship</g-link></li>
+      <li><g-link to="/general">General</g-link></li>
       <li><g-link to="/donation">Donate Here</g-link></li>
     </ul>
   </nav> 
@@ -22,6 +19,16 @@ import Logo from '~/components/Logo'
 export default {
   components: {
     Logo
+  },
+  data() {
+    return {
+      open: false
+    }
+  },
+  methods: {
+    doSomething() {
+      this.open = !this.open;
+    }
   }
 }
 </script>
@@ -33,8 +40,16 @@ header {
   box-shadow: 1px 1px 5px 0 var(--border-color);
 }
 
+.hamburger {
+  float: right;
+  display: none;
+}
+
 .nav-container {
   width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
   
   .logo {
     float: left;
@@ -82,5 +97,37 @@ header {
       }
     }
   }
+}
+
+// Make  things smaller for mobile
+@media screen and (max-width: 650px) {
+  .hamburger {
+    display: block;
+  } 
+
+  .nav-container {
+
+    ul {
+      display: none;
+      background: var(--bg-content-color);
+      padding: 20px;
+      position: absolute;
+      top: 73px;
+      right: 0;
+      z-index: 1;
+      box-shadow: 1px 1px 5px 0 var(--border-color);
+
+      li {
+        float: none;
+      }
+
+      &.open {
+        display: block;
+      } 
+    }
+
+
+  }
+
 }
 </style>
